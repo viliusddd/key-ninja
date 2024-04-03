3b9ef2859373712c1f2cdb6951a52b01d7d45b7a
Vilius <vilius@vilius.net>
2024-04-03 12:10:58 +0300

    Add docstrings to Display class

--- scripts/display/display.js
+++ scripts/display/display.js
@@ -1,8 +1,16 @@
-import { apiUrl, timerTime } from "../config.js"
+import {apiUrl, timerTime} from '../config.js'
 
-let CURRENT_API_URL = ''
+let CURRENT_API_URL = '';
 
+/**
+ * Performs operations on a field that displays words and
+ * stats below it.
+ */
 export default class Display {
+  /**
+   * @param {Element} appElement - root element of the application
+   * @param {Stats} stats - Stats class instance
+   */
   constructor(appElement, stats) {
     this.appElement = appElement
     this.displayElement = appElement.firstElementChild
@@ -12,6 +20,11 @@ export default class Display {
     this.create()
   }
 
+  /**
+   * Gets text from api url, processes it,creates html elements
+   * from it and updates timer element.
+   * @param {string=} url - api url from where the text is taken from
+   */
   async create(url = null) {
     if (!url) url = this.constructUrl(apiUrl, 154)
 
@@ -24,32 +37,40 @@ export default class Display {
   }
 
   /**
+   * Set initial .timer element value.
    * @param {number} time - number in seconds.
    */
   updateTimerElement(time) {
     this.timerElement.innerText = `${time}s`
   }
 
+  /** Reset WPN and Accuracy to their initial values */
   resetStatsElements() {
     this.stats.accElement.innerText = 0
     this.stats.wpmElement.innerText = 0
   }
 
+  /** Remove .word elements, remove .finished & .runs from appElement */
   resetDisplay() {
-    // Remove old .word elements
     this.displayElement.querySelector('.words').replaceChildren()
     this.appElement.classList.remove('finished', 'runs')
   }
 
+  /** Remove chart if it already exists */
   resetChart() {
-    let chartStatus = Chart.getChart("chart")
+    const chartStatus = Chart.getChart('chart')
     if (chartStatus != undefined) {
       chartStatus.destroy()
     }
   }
 
-  restart(reset = null) {
-
+  /**
+   * Restart or reset the app.
+   * If restart is triggered while timer isn't finished - mark as cancel.
+   * @param {boolean} reset - if the value is positive - change
+   * the api url, otherwise leave  the current one.
+   */
+  restart(reset = false) {
     if (this.timerElement.innerText !== '0s') {
       this.appElement.classList.add('cancel')
     }
@@ -65,33 +86,34 @@ export default class Display {
     }
   }
 
-  remElements(query) {
-    const elements = this.displayElement.querySelectorAll(query)
-    elements.forEach(el => el.remove())
-  }
-
   /**
-   * Convert api response json to array of strings.
-   * @param {object} responseJson - json response from api
-   * @return {string[]}
+   * Construct url for random Shakespeare Sonnet.
+   * @param {string} url - api url from config.js.
+   * @param {number} entries - number of Shakespeare Sonnets to choose from.
+   * @return {string}
    */
   constructUrl(url, entries) {
     const randInt = Math.floor(Math.random() * entries) + 1
     return `${url} ${randInt}`
   }
 
+  /**
+   * Create single string containing multiple words from api json output.
+   * @param {object[]} apiJson - array with obejct from api.
+   * @return {string[]} - array of words
+   */
   convertJsonToWords(apiJson) {
     const lines = apiJson[0].lines
     return lines
-      .reduce((accum, line) => `${accum} ${line}`, '')
-      .trim()
-      .split(' ')
-      .filter(word => word)
+        .reduce((accum, line) => `${accum} ${line}`, '')
+        .trim()
+        .split(' ')
+        .filter(word => word)
   }
 
   /**
-   * @param {string} url
-   * @return {Promise<string>}
+   * @param {string} url - url of api containing words for typing.
+   * @return {Promise<object[]>}
    */
   async getApiJson(url) {
     try {
@@ -102,6 +124,11 @@ export default class Display {
     }
   }
 
+  /**
+   * Create div element for each word and letter in it.
+   * Mark 1st word as active.
+   * @param {string[]} words - array of words.
+   */
   buildWords(words) {
     const wordsDiv = this.displayElement.querySelector('.words')
 
@@ -122,6 +149,16 @@ export default class Display {
     })
   }
 
+  /**
+   * Counts seconds backwards from time supplied in config.
+   * It also counts elapsed time to aid in calculating the wpm which is
+   * shown during touch-typing exercise.
+   * Timer stops when either:
+   *   - .app element contains .cancel;
+   *   - .app element contains .restart;
+   *   - timer runs out of time. Only in this case the chart is shown.
+   * @param {Element} appElement - root app element.
+   */
   timer(appElement) {
     let timer = timerTime
     let timeElapsed = 0
